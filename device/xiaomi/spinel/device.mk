6f8d7b5e8362d61b2baa064435056f31e5837517
al5710 <104405399+al5710@users.noreply.github.com>
2026-09-21 08:02:25 +0300

    Update device.mk

--- device/xiaomi/spinel/device.mk
+++ device/xiaomi/spinel/device.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := device/xiaomi/spinel
 
 PRODUCT_COPY_FILES += \
-$(LOCAL_PATH)/recovery/root/init.recovery.mt6789.rc:root/init.recovery.mt6789.rc
+    $(LOCAL_PATH)/recovery/root/init.recovery.mt6789.rc:root/init.recovery.mt6789.rc
 
 PRODUCT_COPY_FILES += \
-$(LOCAL_PATH)/recovery/root/system/etc/recovery.fstab:root/system/etc/recovery.fstab
+    $(LOCAL_PATH)/recovery/root/system/etc/recovery.fstab:root/system/etc/recovery.fstab
