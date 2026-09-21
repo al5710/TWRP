adfb2bc3c8cd89b23668858225f58b23277a83bf
al5710 <104405399+al5710@users.noreply.github.com>
2026-09-21 04:43:51 +0300

    Update device.mk

--- device/xiaomi/spinel/device.mk
+++ device/xiaomi/spinel/device.mk
@@ -5,6 +5,3 @@ $(LOCAL_PATH)/recovery/root/init.recovery.mt6789.rc:root/init.recovery.mt6789.rc
 
 PRODUCT_COPY_FILES += \
 $(LOCAL_PATH)/recovery/root/system/etc/recovery.fstab:root/system/etc/recovery.fstab
-
-PRODUCT_COPY_FILES += \
-$(LOCAL_PATH)/prebuilt/platform_ramdisk.cpio.lz4:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/platform_ramdisk.cpio.lz4
